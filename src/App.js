e32ce1cfae0c71a415dcd7fbfacc5e277595ae74
Valery Anestiadis <anest12mmm@gmail.com>
2022-03-10 22:42:48 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Spectrogram from './components/Spectrogram';
+import Streamgraph from './components/Streamgraph';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container mx-auto'>
+        <Spectrogram />
+        <Streamgraph />
+      </div>
     </div>
   );
 }
